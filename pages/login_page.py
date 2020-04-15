ae86d193546d27bdb181b0cb0db867446b2ff277
Ivanna Sivak <isivak@intel471.com>
2020-04-15 21:03:22 +0300

    Add navigation to the login page for test_guest_can_go_to_login_page test

---
+++ pages/login_page.py
@@ -0,0 +1,22 @@
+from pages.base_page import BasePage
+from pages.locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self, browser):
+        assert "login" in browser.current_url, "It's not a login url"
+        assert self.is_element_present(*LoginPageLocators.EMAIL_ADDRESS_FIELD_LOGIN)
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.EMAIL_ADDRESS_FIELD_LOGIN), "Login form is not present"
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.EMAIL_ADDRESS_FIELD_REGISTER), \
+            "Registration form is not present"
